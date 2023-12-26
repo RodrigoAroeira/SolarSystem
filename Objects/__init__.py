88e556befcf5ead78a9ab363f01644731545f741
RodrigoAroeira <63112121+RodrigoAroeira@users.noreply.github.com>
2023-12-26 13:42:25 +0000

    Add __all__ to Objects.__init__.py

--- Objects/__init__.py
+++ Objects/__init__.py
@@ -5,3 +5,14 @@ from Objects.Satelite import Satelite
 from Objects.Star import Star
 from Objects.System import System
 from Objects.Vector import Vector
+
+
+__all__ = [
+    "Body",
+    "CenterMass",
+    "Planet",
+    "Satelite",
+    "Star",
+    "System",
+    "Vector",
+]
